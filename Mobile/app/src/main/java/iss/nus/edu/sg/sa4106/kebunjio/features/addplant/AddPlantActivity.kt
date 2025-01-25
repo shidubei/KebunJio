b48e958ba5a1a7dd394d00e3a5d154931c6a0f8f
marquan <chuahmark6@gmail.com>
2025-01-25 09:12:11 +0800

    Start Add Plant Activity
    Added permissions needed to send images to the api
    Started on add image UI

--- Mobile/app/src/main/java/iss/nus/edu/sg/sa4106/kebunjio/features/addplant/AddPlantActivity.kt
+++ Mobile/app/src/main/java/iss/nus/edu/sg/sa4106/kebunjio/features/addplant/AddPlantActivity.kt
@@ -1,17 +1,31 @@
 package iss.nus.edu.sg.sa4106.kebunjio.features.addplant
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import iss.nus.edu.sg.sa4106.kebunjio.R
+import iss.nus.edu.sg.sa4106.kebunjio.databinding.ActivityAddPlantBinding
 
 class AddPlantActivity : AppCompatActivity() {
+
+    private var _binding: ActivityAddPlantBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var nameEditText: EditText
+    private lateinit var speciesSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_plant)
+        _binding = ActivityAddPlantBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        nameEditText = binding.nameEditText
+        speciesSpinner = binding.speciesSpinner
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
