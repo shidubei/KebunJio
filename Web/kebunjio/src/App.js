a9cfa79434c6794e509c4856cb1516f97e1c4bbe
MAzrilHello <mazrilhello@gmail.com>
2025-01-25 08:18:40 +0800

    Boilerplate for Web

---
+++ Web/kebunjio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Main web application web-page
+        </p>
+        <a>
+          Welcome to KebunJio web page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Web/kebunjio/src/features/qna-forum/Forum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Q&A Forum</h1>
+      <PostCard />
+    </div>
+  );
+}
+
+export default App;
