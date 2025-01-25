374ad922fc2b1b518afdebb79a17633d3b6dc49b
YasmineK210 <yasminek210@gmail.com>
2025-01-25 14:58:58 +0800

    created folders based on features, added missing dependancies, add basic routing

--- Web/kebunjio/src/App.js
+++ Web/kebunjio/src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardPage from './features/dashboard/dashboard-page';
+import EventPage from './features/event/event-page';
+import ForumPage from './features/qna-forum/forum-page';
+import UserProfilePage from './features/user-profile/user-profile-page';
+import LoginPage from './features/login-signup/login';
+import SignUpPage from './features/login-signup/signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Main web application web-page
-        </p>
-        <a>
-          Welcome to KebunJio web page
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/event" element={<EventPage />} />
+          <Route path="/forum" element={<ForumPage/>} />
+          <Route path="/user-profile" element={<UserProfilePage/>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
