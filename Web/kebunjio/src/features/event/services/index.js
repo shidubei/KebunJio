db5712c3028bff4751b279e2f3ef801bac148b26
WZY <ryanwang24.07.21@gmail.com>
2025-01-28 01:46:01 +0800

    Delete Web/kebunjio/src/features/event/services/index.js

--- Web/kebunjio/src/features/event/services/index.js
+++
@@ -1,3 +0,0 @@
-export { default as EventPage } from './event-page';
-export { default as EventCard } from './components/EventCard';
-export { default as EventDetail } from './components/EventDetail';
