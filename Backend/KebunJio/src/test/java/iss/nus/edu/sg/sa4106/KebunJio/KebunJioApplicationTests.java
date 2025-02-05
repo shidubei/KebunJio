ee0ab1e3e39e66cdd6efba2dce88418ff66721ea
ShiDu <1364136725@qq.com>
2025-02-05 12:43:10 +0800

    testv1.3

--- Backend/KebunJio/src/test/java/iss/nus/edu/sg/sa4106/KebunJio/KebunJioApplicationTests.java
+++ Backend/KebunJio/src/test/java/iss/nus/edu/sg/sa4106/KebunJio/KebunJioApplicationTests.java
@@ -1,13 +1,13 @@
-package iss.nus.edu.sg.sa4106.KebunJio;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KebunJioApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package iss.nus.edu.sg.sa4106.KebunJio;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KebunJioApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- Backend/KebunJio/src/test/java/iss/nus/edu/sg/sa4106/KebunJio/ServiceTest/PostServiceTest.java
+++ Backend/KebunJio/src/test/java/iss/nus/edu/sg/sa4106/KebunJio/ServiceTest/PostServiceTest.java
@@ -1,20 +1,20 @@
-package iss.nus.edu.sg.sa4106.KebunJio.ServiceTest;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import iss.nus.edu.sg.sa4106.KebunJio.Services.PostService;
-
-@SpringBootTest
-public class PostServiceTest {
-	@Autowired
-	private PostService postService;
-	
-	@Test
-	void testAdd() {
-		int result = postService.add(1, 2);
-		Assertions.assertEquals(3,result);
-	}
-}
+//package iss.nus.edu.sg.sa4106.KebunJio.ServiceTest;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import iss.nus.edu.sg.sa4106.KebunJio.Services.PostService;
+//
+//@SpringBootTest
+//public class PostServiceTest {
+//	@Autowired
+//	private PostService postService;
+//	
+//	@Test
+//	void testAdd() {
+//		int result = postService.add(1, 2);
+//		Assertions.assertEquals(3,result);
+//	}
+//}
